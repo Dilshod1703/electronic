5a47fdb5705ccf35d2401a0427df08c841ddc612
Dilshod1703 <jabborberganov2001@gmail.com>
2023-03-30 22:31:39 +0500

    Can not add a user through postman

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
-import dotenv from "dotenv"
-import connectToDatabase from "./database.js"
-import express from "express"
+import dotenv from 'dotenv';
+import connectToDatabase from './database.js';
+import express from 'express';
 
 //Routes
-import productRoutes from "./routes/productRoutes.js"
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
-dotenv.config()
+dotenv.config();
 
-connectToDatabase()
+connectToDatabase();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use("/api/products", productRoutes)
+app.use('/api/products', productRoutes);
+app.use('api/users', userRoutes);
 
 app.listen(port, () => {
-  console.log(`Server runs on port ${port}`)
-})
+  console.log(`Server runs on port ${port}`);
+});
